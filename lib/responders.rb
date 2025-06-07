8eff005cdba4bcb52924d0524232d9efd3efab3b
zaMmer <alexzammer@gmail.com>
2025-06-08 00:58:49 +0300

    Fix exceptions with error_status and redirect_status

--- lib/responders.rb
+++ lib/responders.rb
@@ -29,8 +29,8 @@ module Responders
     initializer "responders.flash_responder" do |app|
       Responders::FlashResponder.flash_keys = app.config.responders.flash_keys
       Responders::FlashResponder.namespace_lookup = app.config.responders.namespace_lookup
-      ActionController::Responder.error_status = app.config.responders.error_status
-      ActionController::Responder.redirect_status = app.config.responders.redirect_status
+      # ActionController::Responder.error_status = app.config.responders.error_status
+      # ActionController::Responder.redirect_status = app.config.responders.redirect_status
     end
   end
 end
